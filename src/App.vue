e5db3cfba48bc89e25c681a0aacb2dded6ac9c84
JK82NL <jk-dance@hotmail.com>
2025-06-24 20:41:04 +0200

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Counter from "./components/Counter.vue";
+import { ref } from "vue";
+
+const counters = ref<string[]>([]);
+const newName = ref("");
+
+function handleThreshold(value: string) {
+  alert(value);
+}
+
+function addCount() {
+  counters.value.push(newName.value);
+  newName.value = "";
+}
+</script>
+
+<template>
+  <h1>Dit is mijn teller</h1>
+  <!-- <input type="text" v-model="newName" />{{ newName }} UPDATE GELIJK-->
+  <input type="text" v-model="newName" />
+  <button @click="addCount">Nieuwe teller</button>
+  <!-- <Counter :name="'Koolhydraten' + 'iets anders'" @threshold-reached="handleThreshold"></Counter>
+  <Counter name="Sugars"></Counter> -->
+  <div v-for="count in counters">
+    <Counter :name="count" @threshold-reached="handleThreshold"></Counter>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{
+  name?: string;
+}>();
+// name?: geeft aan dat een naam optioneels is.
+const emit = defineEmits<{
+  (event: "thresholdReached", value: string): void;
+}>();
+
+const count = ref(0);
+
+function updateCount(value: number) {
+  count.value += value;
+
+  if (count.value > 10) {
+    emit("thresholdReached", "Hyper Bereikt!");
+  }
+}
+</script>
+
+<template>
+  <p>{{ name }}</p>
+  <div>{{ count }}</div>
+  <button @click="updateCount(1)">+</button>
+  <button @click="updateCount(-1)">-</button>
+  <button v-if="count !== 0" @click="count = 0">Reset</button>
+</template>
+
+<style scoped></style>
