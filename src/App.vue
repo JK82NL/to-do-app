85009e46115a6baee35cf8c04cfbbfebb173eb01
JK82NL <jk-dance@hotmail.com>
2025-06-26 15:21:54 +0200

    Started creatingtitle header and an input field to add tasks. Created scoped css container for with centering

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
 <script setup lang="ts">
-import Counter from "./components/Counter.vue";
+import ToDoListInput from "./components/ToDoListInput.vue";
 import { ref } from "vue";
 
-const counters = ref<string[]>([]);
-const newName = ref("");
+// import Counter from "./components/Counter.vue";
+// import { ref } from "vue";
 
-function handleThreshold(value: string) {
-  alert(value);
-}
+// const counters = ref<string[]>([]);
+// const newName = ref("");
 
-function addCount() {
-  counters.value.push(newName.value);
-  newName.value = "";
+const giveName = ref<string[]>();
+const addTask = ref("");
+
+function addTaskList() {
+  giveName.value.push(giveName.value);
 }
+// function handleThreshold(value: string) {
+//   alert(value);
+// }
+
+// function addCount() {
+//   counters.value.push(newName.value);
+//   newName.value = "";
+// }
 </script>
 
 <template>
-  <h1>Dit is mijn teller</h1>
+  <header class="container"><h1>To-do App</h1></header>
+
+  <div class="container">
+    <input type="text" v-model="giveName" />
+    <button @click="addTask">Add Task</button>
+  </div>
+  <div class="container">{{ giveName }}</div>
+
+  <!--  -->
+  <!-- <h1>Dit is mijn teller</h1> -->
   <!-- <input type="text" v-model="newName" />{{ newName }} UPDATE GELIJK-->
-  <input type="text" v-model="newName" />
-  <button @click="addCount">Nieuwe teller</button>
+  <!-- <input type="text" v-model="newName" /> -->
+  <!-- <button @click="addCount">Nieuwe teller</button> -->
   <!-- <Counter :name="'Koolhydraten' + 'iets anders'" @threshold-reached="handleThreshold"></Counter>
   <Counter name="Sugars"></Counter> -->
-  <div v-for="count in counters">
-    <Counter :name="count" @threshold-reached="handleThreshold"></Counter>
-  </div>
+  <!-- <div v-for="count in counters"> -->
+  <!-- <Counter :name="count" @threshold-reached="handleThreshold"></Counter> -->
+  <!-- </div> -->
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 15px;
+}
+
+header {
+  background: lightcoral;
+}
+
+h1 {
+  font-size: xx-large;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ src/components/ToDoListInput.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  name?: string;
+}>();
+</script>
+
+<template>
+  <p>{{ name }}</p>
+</template>
+
+<style scoped></style>
